b9a11d00fab74c465e5e51991b8837327d663fb7
DCRevResLabs <dc@revolutionresearch.com.au>
2020-06-20 21:22:01 +1000

    Updates

---
+++ precache-manifest.e169a04ce3447f8793a8837b5551d300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ae27c73359a5aed85517856c7f04d2",
+    "url": "/employee-tracker-react/index.html"
+  },
+  {
+    "revision": "20e2d7d21c4e76bbf889",
+    "url": "/employee-tracker-react/static/css/main.887cdf7c.chunk.css"
+  },
+  {
+    "revision": "302eded694bb63b76acd",
+    "url": "/employee-tracker-react/static/js/2.e377a6bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employee-tracker-react/static/js/2.e377a6bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e2d7d21c4e76bbf889",
+    "url": "/employee-tracker-react/static/js/main.93e49765.chunk.js"
+  },
+  {
+    "revision": "114b6db9145d66611703",
+    "url": "/employee-tracker-react/static/js/runtime-main.2b1a99b8.js"
+  }
+]);
